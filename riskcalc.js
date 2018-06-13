c57f91f0096764c3d0870aa5a79a22587653d886
Johannes Teichert <15386957+JohannesTeichert@users.noreply.github.com>
2018-06-13 18:36:05 +0200

    First commit

---
+++ riskcalc.js
@@ -0,0 +1,12 @@
+M.AutoInit();
+
+function calcTobacco(value, rr){
+	let result = 0;
+	let inputs = document.getElementsByClassName("tobacco");
+	for (i=0; i<inputs.length;i++){
+		if (inputs[i].checked){
+			result+=parseFloat(inputs[i].dataset.rr);
+		}
+	}
+	document.getElementById("tobaccoResult").innerHTML="Tobacco induced risk for oral cancer: "+result;
+}
