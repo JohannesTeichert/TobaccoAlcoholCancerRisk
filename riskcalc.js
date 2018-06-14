ffd94fd4d4ec818be6806519721796a550e3d026
Johannes Teichert <15386957+JohannesTeichert@users.noreply.github.com>
2018-06-14 07:36:10 +0200

    added alcohol relative risk calculation functionality

--- riskcalc.js
+++ riskcalc.js
@@ -1,12 +1,38 @@
 M.AutoInit();
 
+let resultTobacco = 0.0;
+let resultAlc = 0.0;
+
 function calcTobacco(value, rr){
-	let result = 0;
+	resultTobacco = 0.0;
 	let inputs = document.getElementsByClassName("tobacco");
 	for (i=0; i<inputs.length;i++){
 		if (inputs[i].checked){
-			result+=parseFloat(inputs[i].dataset.rr);
+			resultTobacco+=parseFloat(inputs[i].dataset.rr);
 		}
 	}
-	document.getElementById("tobaccoResult").innerHTML="Tobacco induced risk for oral cancer: "+result.toFixed(1);
+	document.getElementById("tobaccoResult").innerHTML="Tobacco induced risk for oral cancer: "+resultTobacco.toFixed(1);
+	checkCombined();
+}
+
+
+function calcAlcohol(){
+	resultAlc = 0.0;
+	let inputs = document.getElementsByClassName("alcohol");
+	for (i=0; i<inputs.length;i++){
+		if (inputs[i].checked){
+			resultAlc = parseFloat(inputs[i].dataset.oral);
+			document.getElementById("oral").innerHTML="Oral and pharyngeal cancer: "+parseFloat(inputs[i].dataset.oral)
+			document.getElementById("laryngeal").innerHTML="Laryngeal cancer: "+parseFloat(inputs[i].dataset.laryngeal)
+			document.getElementById("esophageal").innerHTML="Esophageal cancer: "+parseFloat(inputs[i].dataset.esophageal)
+			document.getElementById("liver").innerHTML="Liver cancer: "+parseFloat(inputs[i].dataset.liver)
+		}
+	}
+	checkCombined();
+}
+
+function checkCombined(){
+	if (resultTobacco > 0 && resultAlc > 0){
+		document.getElementById("oral").innerHTML="Oral and pharyngeal cancer: \nCombined risk of "+resultAlc+" x "+resultTobacco+" = "+(resultAlc*resultTobacco).toFixed(2);
+	}
 }
